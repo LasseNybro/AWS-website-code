21090df09e5cda6ed0af58d4019a541b70a7772d
LasseNybro <lasso290391@hotmail.com>
2025-03-09 02:49:07 +0100

    initial commit

---
+++ src/Menu.tsx
@@ -0,0 +1,15 @@
+import './Menu.css';
+
+function App() {
+  return (
+    <div className="Menu">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
